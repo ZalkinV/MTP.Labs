50c269b12ed5e46ac0542fb60d4bca3bae255764
Zalkin <zalkin.viktor@mail.ru>
2021-03-29 20:12:15 +0300

    L2. Create Lab2AutoBrightness

---
+++ Lab2AutoBrightness/Main.cpp
@@ -0,0 +1,10 @@
+/*
+Useful links:
+https://docs.google.com/presentation/d/1pH7sGzIy2-_qd6YYeApTmw5vHwxjV9Y2CwvRAQ82EIc/edit
+https://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
+*/
+
+int main(int argc, char* agrv[])
+{
+	return 0;
+}
