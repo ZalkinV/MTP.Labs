436b2a77c8edd026a537e2c4431a4c0a2c1053b7
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-04-20 15:15:30 +0300

    P4/MO. Add multiply() for matrix in 1D array

--- Prac4OpenCLMatrix/MatrixOperations.cpp
+++ Prac4OpenCLMatrix/MatrixOperations.cpp
@@ -23,3 +23,28 @@ int** multiply(int** matrixA, int** matrixB, int rowsCount, int colsCount, int e
 
 	return matrixRes;
 }
+
+int* multiply(int* matrixA, int* matrixB, int rowsCount, int colsCount, int elsCount)
+{
+	int* matrixRes = new int[rowsCount * colsCount];
+
+	for (int iRow = 0; iRow < rowsCount; iRow++)
+	{
+		for (int iCol = 0; iCol < colsCount; iCol++)
+		{
+			int elSum = 0;
+			for (int iEl = 0; iEl < elsCount; iEl++)
+			{
+				int indexA = iRow * rowsCount + iEl;
+				int indexB = iEl * colsCount + iCol;
+				int elMul = matrixA[indexA] * matrixB[indexB];
+				elSum += elMul;
+			}
+
+			int indexRes = iRow * rowsCount + iCol;
+			matrixRes[indexRes] = elSum;
+		}
+	}
+
+	return matrixRes;
+}
--- Prac4OpenCLMatrix/MatrixOperations.h
+++ Prac4OpenCLMatrix/MatrixOperations.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int** multiply(int** matrixA, int** matrixB, int rowsCount, int colsCount, int elsCount);
+int* multiply(int* matrixA, int* matrixB, int rowsCount, int colsCount, int elsCount);
