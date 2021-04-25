4a35ea8a84a4ac0b28e57d22bf8eeb18c5245b7f
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-04-25 15:53:40 +0300

    P4/M. Add calling Mul.ocl kernel but it fails with -5. Integrated GPU freezes and restarting

--- Prac4OpenCLMatrix/Main.cpp
+++ Prac4OpenCLMatrix/Main.cpp
@@ -82,6 +82,67 @@ void runMulKernel(cl_context context, cl_device_id deviceId, cl_command_queue qu
 	int* matrixB = convertTo1D(matrixB2D, elsCount, colsCount);
 
 	int* matrixCorrectResult = multiply(matrixA, matrixB, rowsCount, colsCount, elsCount);
+
+
+	const char* kernelName = "matrixMul";
+	cl_program program = getProgram(context, "Mul.ocl");
+	int buildStatus = buildProgram(program, deviceId);
+
+	cl_int err = 0;
+
+	cl_kernel kernel = clCreateKernel(program, kernelName, &err);
+
+	size_t bufferASize = rowsCount * elsCount * sizeof(int);
+	cl_mem bufferA = clCreateBuffer(context, CL_MEM_READ_ONLY, bufferASize, NULL, &err);
+	err = clEnqueueWriteBuffer(queue, bufferA, false, 0, bufferASize, matrixA, NULL, NULL, NULL);
+
+	size_t bufferBSize = elsCount * colsCount * sizeof(int);
+	cl_mem bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY, bufferBSize, NULL, &err);
+	err = clEnqueueWriteBuffer(queue, bufferB, false, 0, bufferBSize, matrixB, NULL, NULL, NULL);
+	
+	size_t matrixCSize = rowsCount * colsCount;
+	size_t bufferCSize = matrixCSize * sizeof(int);
+	cl_mem bufferC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bufferCSize, NULL, &err);
+
+	cl_uint iArg = 0;
+	err = clSetKernelArg(kernel, iArg++, sizeof(cl_mem), &bufferA);
+	err = clSetKernelArg(kernel, iArg++, sizeof(cl_mem), &bufferB);
+	err = clSetKernelArg(kernel, iArg++, sizeof(int), &colsCount);
+	err = clSetKernelArg(kernel, iArg++, sizeof(int), &elsCount);
+	err = clSetKernelArg(kernel, iArg++, sizeof(cl_mem), &bufferC);
+
+
+	cl_uint workDim = 2;
+	size_t globalWorkSize = matrixCSize;
+	cl_event kernelStartEvent;
+	err = clEnqueueNDRangeKernel(queue, kernel, workDim, NULL, &globalWorkSize, NULL, NULL, NULL, &kernelStartEvent);
+
+
+	int* matrixC = new int[matrixCSize];
+	err = clEnqueueReadBuffer(queue, bufferC, true, NULL, bufferCSize, matrixC, NULL, NULL, NULL);
+
+	clReleaseMemObject(bufferA);
+	clReleaseMemObject(bufferB);
+	clReleaseMemObject(bufferC);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+
+	for (int iRow = 0; iRow < rowsCount; iRow++)
+	{
+		for (int iCol = 0; iCol < colsCount; iCol++)
+		{
+			int indexC = iRow * colsCount + iCol;
+			printf("%i ", matrixC[indexC]);
+		}
+		printf("\n");
+	}
+
+	delete[] matrixA;
+	delete[] matrixB;
+	delete[] matrixC;
+
+	int kernelExecTime = getElapsedTimeUs(kernelStartEvent);
+	printf("Kernel execution time: %i us\n", kernelExecTime);
 }
 
 int main()
