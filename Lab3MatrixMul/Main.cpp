2d5598e7a11439a06a8e4e153c6c0896b8eaaa41
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-05-03 12:25:31 +0300

    L3/M. Add reading input parameters

--- Lab3MatrixMul/Main.cpp
+++ Lab3MatrixMul/Main.cpp
@@ -1,11 +1,43 @@
 #define CL_TARGET_OPENCL_VERSION 120
 
-#include "MatrixMulImpls.h"
+#include <exception>
 
+#include "MatrixMulImpls.h"
+#include <string>
+
+using namespace std;
+
+int getDeviceIndex(char* str)
+{
+	int parsedValue = stoi(str);
+
+	if (parsedValue < 0 || MAX_DEVICES_COUNT <= parsedValue)
+		throw runtime_error("deviceIndex cannot be less than 0 and inclusively greater than " + to_string(MAX_DEVICES_COUNT));
+
+	return parsedValue;
+}
+
+int getImplementationNumber(char* str)
+{
+	int parsedValue = stoi(str);
+
+	if (parsedValue < 1 || 3 < parsedValue)
+		throw runtime_error("Implementation number cannot be less than 1 and greater than 3");
+
+	return parsedValue;
+}
 
 int main(int argc, char* argv[])
 {
-	cl_device_id deviceId = getDeviceId(0);
+	if (argc < 5)
+		throw exception("Wrong count of arguments");
+
+	int deviceIndex = getDeviceIndex(argv[1]);
+	char* inputFilename = argv[2];
+	char* outputFilename = argv[3];
+	int implementationNumber = getImplementationNumber(argv[4]);
+
+	cl_device_id deviceId = getDeviceId(deviceIndex);
 
 	cl_context context = clCreateContext(NULL, 1, &deviceId, NULL, NULL, NULL);
 
